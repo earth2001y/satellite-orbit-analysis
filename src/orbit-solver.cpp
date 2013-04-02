aec8732c1d52fce459a57f04968495d355c6a6fe
Daichi OBINATA <earth2001y@gmail.com>
2013-04-02 18:08:09 +0900

    計算に使う物理/数学パラメータとなる定数,変数を追加
    - Spacetrack Report No.3 を参考に
    - 一部定数はGSLを使用

--- src/orbit-solver.cpp
+++ src/orbit-solver.cpp
@@ -1,9 +1,20 @@
 
+#include <cmath>
+
 #include "tle.hpp"
 #include "orbit-solver.hpp"
 
 orb_solver::orb_solver()
 {
+  k2  = CK2;
+  k4  = CK4;
+  aE  = XKMPER;
+//ke  = sqrt(G*M);
+  ke  = sqrt(GE);
+  J2  = 2.0*k2/pow(aE,2);        // because, k2 = (1/2)J2.aE**2
+  J3  = XJ3;
+  J4  = -(8.0/3.0)*k4/pow(aE,4); // because, k4 = -(3/8)J4.aE**4
+  A30 = -J3*pow(aE,3);
 }
 
 orb_solver::~orb_solver()
@@ -13,6 +24,13 @@ orb_solver::~orb_solver()
 int orb_solver::setTLE(const TLE* tle)
 {
   orb_solver::tle = tle;
+
+  n0  = tle->motion;
+  e0  = tle->eccentricity;
+  i0  = tle->inclination;
+  M0  = tle->anomaly;
+  w0  = tle->perigee;
+
   return 0;
 }
 
--- src/orbit-solver.hpp
+++ src/orbit-solver.hpp
@@ -2,6 +2,8 @@
  * 衛星の位置・速度計算ソルバの基底クラス
  *****************************************************************************/
 
+#include <gsl/gsl_const_mksa.h>
+
 #include "tle.hpp"
 
 #ifndef __ORBIT_SOLVER_HPP__
@@ -14,6 +16,56 @@
 
 class orb_solver {
   const TLE* tle;
+
+  // physical and mathematical constants.
+  static const double CK2    = 5.413080E-4;    // (1/2)J2.aE**2, equiv k2
+  static const double CK4    = .62098875E-6;   // -(3/8)J4.aE**4, equiv k4
+  static const double E6A    = 1.0E-6;
+  static const double QOMS2T = 1.88027916E-9;  // (q0-s)**4 * (er)**4
+  static const double S      = 1.01222928;     // s(er)
+  static const double TOTHRD = .66666667;      // 2/3
+  static const double XJ3    = -.253881E-5;    // J3
+  static const double XKE    = .74366916E-1;   // ke(er/min)**(3/2)
+  static const double XKMPER = 6378.135;       // Earth radii [km]
+  static const double XMNPDA = 1440.0;         // time units per day
+  static const double AE     = 1.0;            // distance units/Earth radii
+  static const double DE2RA  = .174532925E-1;  // radians/degree
+  static const double PI     = 3.14159265;     // pi
+  static const double PIO2   = 1.57079633;     // pi/2
+  static const double TWOPI  = 6.2831853;      // 2.pi
+  static const double X3PIO2 = 4.71238898;     // 3.pi/2
+//static const double PI     = M_PI;           // pi
+//static const double PIO2   = M_PI_2;         // pi/2
+//static const double TWOPI  = 2.0*M_PI;       // 2.pi
+//static const double X3PIO2 = 3.0*M_PI_2;     // 3.pi/2
+  static const double M      = 5.9736E24;      // mass of the Earth [kg] (by NASA)
+  static const double G      = 6.67384E-11;    // gravitational constant [m3.s-2.kg-1]
+  static const double GE     = 3.986004418E14; // equiv G*M [m3.s-2]
+//static const double G      = GSL_CONST_MKSA_GRAVITATIONAL_CONSTANT;
+
+  // orbital parameters set
+  double n0;   // the SGP type "meean" mean motion at epoch
+  double e0;   // the "mean" eccentricity at epoch
+  double i0;   // the "mean" inclination at epoch
+  double M0;   // the "mean" mean anomaly at epoch
+  double w0;   // the "mean" argument of perigee at epoch
+  double om0;  // the "mean" longitude of ascending node at epoch
+  double dn0;  // the time rate of change of "mean" mean motion at epoch
+  double ddn0; // the second time rate of change of "mean" mean motion at epoch
+  double Bc;   // the SGP4 type drag coefficient
+  double ke;   // sqrt(G.M) where G is Newton's universal gravitational constant and M is the mass of the Earth
+  double aE;   // the equatorial radius of the Earth
+  double J2;   // the second gravitational zonal harmonic of the Earth
+  double J3;   // the third gravitational zonal harmonic of the Earth
+  double J4;   // the fourth gravitational zonal harmonic of the Earth
+  double k2;   // k2 = (1/2)J2.aE**2
+  double k4;   // k4 = -(3/8)J4.aE**4
+  double A30;  // A30 = -J3.aE**3
+  double qo;   // parameter for the SGP4/SGP8 density function
+  double s;    // parameter for the SGP4/SGP8 density function
+  double B;    // B = (1/2)CD(A/m)
+               //   the ballistic coefficient for SGP8 where CD is a dimensionless drag
+               //   coefficient and A is the average cross-sectional area of the satellite of mass m
 
 public:
   orb_solver();
